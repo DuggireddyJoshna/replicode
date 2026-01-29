d35d63f19876ae727bdc4cd00b09a64d262b7ad4
DuggireddyJoshna <duggireddyjoshna@gmail.com>
2026-01-29 14:49:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  toggleBtn.textContent = document.body.classList.contains("dark-mode")
+    ? "☀️"
+    : "🌙";
+});
